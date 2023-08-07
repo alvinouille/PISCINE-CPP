caa4cb35ebe4196fc3770ff49a9f4471f931b6bc
Alvina LE SAINT <ale-sain@paul-f5Br6s7.clusters.42paris.fr>
2023-08-07 12:45:20 +0200

    cpp09 apparently ok, first push

--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -10,69 +10,95 @@
 #include <sstream>
 #include <sys/time.h>
 
+# define BEFORE 0
+# define AFTER 1
+
 template <typename T>
 class PmergeMe
 {
     public:
-        PmergeMe() {}
-        PmergeMe(PmergeMe const &copy) {(void) copy;}
+        PmergeMe(int size, char **nb)
+        {
+            for (int i = 1 ; i < size ; i++)
+            {
+                int value = PmergeMe::digitValue(std::string(nb[i]));
+                if (value == -1)
+                {
+                    std::cout << "Error: Wrong input" << std::endl;
+                    return ;
+                }
+                this->_cont.push_back(value);
+            }
+        }
+        PmergeMe(T const &cont) : _cont(cont) {}
+        PmergeMe(PmergeMe const &copy) : _cont(copy._cont), _begin(copy._begin), _end(copy._end) {(void) copy;}
         ~PmergeMe() {}
         PmergeMe &operator=(PmergeMe const &rhs)
         {
             if (this == &rhs)
                 return (*this);
+            this->_cont = rhs._cont;
+            this->_begin = rhs._begin;
+            this->_end = rhs._end;
             return (*this);
         }
 
-        void insertionSort(T &vec)
+        void insertionSort()
         {
-            int tmp;
-            for (size_t i = 1 ; i < vec.size() ; i++)
+            unsigned int tmp;
+            for (size_t i = 1 ; i < this->_cont.size() ; i++)
             {
-                if (vec[i - 1] > vec[i])
+                if (this->_cont[i - 1] > this->_cont[i])
                 {
-                    tmp = vec[i];
-                    while (i > 0 && vec[i - 1] > tmp)
+                    tmp = this->_cont[i];
+                    while (i > 0 && this->_cont[i - 1] > tmp)
                     {
-                        vec[i] = vec[i - 1];
+                        this->_cont[i] = this->_cont[i - 1];
                         i--;
                     }
-                    vec[i] = tmp;
+                    this->_cont[i] = tmp;
                 }
             }
         }
 
-        T fusionSort(T &vec1, T &vec2)
+        T fusionSort(T &_cont1, T &_cont2)
         {
             size_t j = 0;
             size_t k = 0;
             size_t i = 0;
-            size_t vsize = vec1.size() + vec2.size();
-            T vec(vsize);
+            size_t vsize = _cont1.size() + _cont2.size();
+            T _cont(vsize);
             
-            for (i = 0 ; j != vec1.size() && k != vec2.size() ; i++)
-                vec[i] = (vec1[j] < vec2[k]) ? vec1[j++] : vec2[k++];
+            for (i = 0 ; j != _cont1.size() && k != _cont2.size() ; i++)
+                _cont[i] = (_cont1[j] < _cont2[k]) ? _cont1[j++] : _cont2[k++];
             while (i < vsize)
-                vec[i++] = (k == vec2.size() ? vec1[j++] : vec2[k++]);
-            return (vec);
+                _cont[i++] = (k == _cont2.size() ? _cont1[j++] : _cont2[k++]);
+            return (_cont);
         }
 
-        void divideToReign(T &vec)
+        void divideToReign()
         {
-            size_t middle = vec.size() / 2;
-            T vec1(vec.begin(), vec.begin() + middle);
-            T vec2(vec.begin() + middle, vec.end());
-            fordJohnsonSort(vec1);
-            fordJohnsonSort(vec2);
-            vec = fusionSort(vec1, vec2);
+            size_t middle = this->_cont.size() / 2;
+            T _cont1(_cont.begin(), _cont.begin() + middle);
+            T _cont2(_cont.begin() + middle, _cont.end());
+            PmergeMe <T> c1(_cont1);
+            PmergeMe <T> c2(_cont2);
+            c1.fordJohnsonSort();
+            c2.fordJohnsonSort();
+            this->_cont = fusionSort(c1.getCont(), c2.getCont());
         }
 
-        void fordJohnsonSort(T &vec)
+        void fordJohnsonSort()
         {
-            if (vec.size() <= 32)
-                insertionSort(vec);
+            if (this->_cont.size() <= 32)
+                insertionSort();
             else
-                divideToReign(vec);
+                divideToReign();
+        }
+
+        T &getCont()
+        {
+            return (this->_cont);
         }
 
         void startChrono(std::clock_t begin)
@@ -90,9 +116,40 @@ class PmergeMe
             return (static_cast<double>(this->_end - this->_begin) / static_cast<double>(CLOCKS_PER_SEC) * 1000.0);
         }
 
-        private:
-            std::clock_t _begin;
-            std::clock_t _end;
+        void printCont(int time)const
+        {
+            std::cout << (time == BEFORE ? "<<< BEFORE >>> :     " : "<<< AFTER >>>  :     ");
+            for (size_t i = 0 ; i < this->_cont.size() ; i++)
+                std::cout << this->_cont[i] << " ";
+            std::cout << std::endl;
+        }
+
+        void printChrono(std::string type)const
+        {
+            std::cout << std::endl << "Time to process a range of " << this->_cont.size();
+            std::cout << " elements with " << type << " : ";
+            std::cout.precision(6);
+            std::cout << this->resultChrono() << " ms" << std::endl;
+        }
+
+        static int digitValue(std::string value)
+        {
+            unsigned int nb;
+            std::istringstream iss(value);
+            for (size_t i = 0 ; i < value.size() ; i++)
+            {
+                if (!std::isdigit(value[i]))
+                    return (-1);
+            }
+            iss >> nb;
+            return (nb);
+        }
+
+    private:
+        std::clock_t _begin;
+        std::clock_t _end;
+        T _cont;
+        PmergeMe();
 };
 
 #endif
--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -1,59 +1,27 @@
 #include "PmergeMe.hpp"
 
-static int digitValue(std::string value)
-{
-    unsigned int nb;
-    std::istringstream iss(value);
-    for (size_t i = 0 ; i < value.size() ; i++)
-    {
-        if (!std::isdigit(value[i]))
-            return (-1);
-    }
-    iss >> nb;
-    return (nb);
-}
-
 int main(int ac, char **av)
 {
-    PmergeMe <std::vector<int> > pmV;
-    PmergeMe <std::deque<int> > pmD;
-    std::vector<int> vec;
-    std::deque<int> dq;
+    // <<<<<< VECTORS SORT >>>>>>>
+    PmergeMe<std::vector<unsigned int> > pmV(ac, av);
 
-    for (int i = 1 ; i < ac ; i++)
-    {
-        int value = digitValue(std::string(av[i]));
-        if (value == -1)
-            return (std::cout << "Error: Wrong input" << std::endl), 1;
-        vec.push_back(value);
-        dq.push_back(value);
-    }
-
-    std::cout << std::endl << "<<< BEFORE >>> :     ";
-    for (size_t i = 0 ; i < vec.size() ; i++)
-        std::cout << vec[i] << " ";
-    std::cout << std::endl;
+    pmV.printCont(BEFORE);
 
     pmV.startChrono(std::clock());
-    pmV.fordJohnsonSort(vec);
+    pmV.fordJohnsonSort();
     pmV.endChrono(std::clock());
 
+    pmV.printCont(AFTER);
+
+    pmV.printChrono("std::vector<unsigned int>");
+
+
+    // <<<<<<< DEQUES SORT >>>>>>>
+    PmergeMe<std::deque<unsigned int> > pmD(ac, av);
+
     pmD.startChrono(std::clock());
-    pmD.fordJohnsonSort(dq);
+    pmD.fordJohnsonSort();
     pmD.endChrono(std::clock());
 
-    std::cout << "<<< AFTER >>> :      ";
-    for (size_t i = 0 ; i < vec.size() ; i++)
-        std::cout << vec[i] << " ";
-    std::cout << std::endl;
-
-    std::cout << std::endl << "Time to process a range of " << vec.size();
-    std::cout <<  " elements with std::vector : " ;
-    std::cout.precision(6);
-    std::cout << pmV.resultChrono() << " ms" << std::endl;
-
-    std::cout << "Time to process a range of " << dq.size();
-    std::cout <<  " elements with std::deque : ";
-    std::cout.precision(6);
-    std::cout << pmD.resultChrono() << " ms" << std::endl;
+    pmD.printChrono("std::deque<unsigned int>");
 }
